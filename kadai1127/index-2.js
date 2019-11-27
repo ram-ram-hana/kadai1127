847d56b220e08b5b6ab5c7f0d05c3b594c2795b9
ram-ram-hana <ram.ram.hana@gmail.com>
2019-11-27 05:07:48 +0000

    課題を追加

--- kadai1127/index-2.js
+++ kadai1127/index-2.js
@@ -5,6 +5,7 @@ const str4 = '{"a": undefined}'
 const str5 = '{"a": 123}'
 const str6 = '{a: 123}'
 
+ 　  //str1　のパース結果
 try {
     console.log(JSON.parse(str1))
 } catch (error) {
@@ -13,4 +14,48 @@ try {
 　　// 正しくは　"abc"
 }
 
+
+    //str2 のパース結果
+try {
+    console.log(JSON.parse(str2))
+} catch (error) {
+    console.log(error)
+}
+
+　　　//str3 のパース結果
+try {
+    console.log(JSON.parse(str3))
+} catch (error) {
+    console.log(error)
+}
+
+
+     //str4　のパース結果
+try {
+    console.log(JSON.parse(str4))
+} catch (error) {
+    console.log(error)
+}
+
+
+      //str5 のパース結果
+try {
+    console.log(JSON.parse(str5))
+} catch (error) {
+    console.log(error)
+}
+
+       //str6　のパース結果
+try {
+    console.log(JSON.parse(str6))
+} catch (error) {
+    console.log(error)
+}
+
+
+console.log(JSON.parse(str1))
+console.log(JSON.parse(str2))
 console.log(JSON.parse(str3))
+console.log(JSON.parse(str4))
+console.log(JSON.parse(str5))
+console.log(JSON.parse(str6))
