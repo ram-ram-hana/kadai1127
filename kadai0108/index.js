d22db009711ffa6234b8886c68a26da9dfa5aeda
ram-ram-hana <ram.ram.hana@gmail.com>
2020-01-08 05:30:50 +0000

    kadai

--- kadai0108/index.js
+++ kadai0108/index.js
@@ -1,5 +1,11 @@
 const http = require('http');
+const url = require('url')
+
+
 const server = http.createServer((req, res) => {
+     const parsedUrl = url/parsedUrl(req.url)
+     console.log(req.url)
+    res.setHeader('Access-Control-Allow-Origin', '*');
     res.write('hello');
     res.end();
     });
